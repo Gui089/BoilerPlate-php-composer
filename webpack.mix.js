3eecb0c389baabe803f0a4a145bf0cba4a74ceff
Gui089 <guilhermesilba811@gmail.com>
2024-12-07 15:58:48 -0300

    config: tailwindcss cli

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,6 @@ const mix = require('laravel-mix');
 
 mix.setPublicPath('public');
 
-mix.css('resources/css/app.css', 'public/styles');
+mix.css('resources/css/app.css', 'public/styles', [
+    require('tailwindcss'),
+]);
