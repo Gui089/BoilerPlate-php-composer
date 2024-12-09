0139ce200262f28be06dac82e6bfd1ed9de56bf2
Gui089 <guilhermesilba811@gmail.com>
2024-12-09 09:08:05 -0300

    feat: add view contact

--- public/index.php
+++ public/index.php
@@ -7,8 +7,9 @@ $products = Product::all();
 
 $filteredProducts = array_filter($products, static fn(array $product) => $product['Is_available'] == true);
 $title = "my web store";
+$heading = "Home";
 
-require __DIR__."/../resources/views/index.phtml";
+require __DIR__ . "/../resources/views/index.php";
 
 ?>
 
---
+++ resources/views/includes/head.php
@@ -0,0 +1,11 @@
+<html lang="en" class="h-full bg-gray-100">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <title> <?= $title ?></title>
+    <!--<link rel="stylesheet" href="/styles/app.css">-->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="h-full ">
+<div class="min-h-full">
+
