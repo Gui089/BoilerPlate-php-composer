eb23ed479ee8771c1664c301a966b7528cd4068f
Gui089 <guilhermesilba811@gmail.com>
2024-12-09 09:17:31 -0300

    feat: add Home Controller and Contact Controller

---
+++ src/Controllers/contact.php
@@ -0,0 +1,9 @@
+<?php
+
+$title = "Contact Us";
+$heading = $title;
+
+require __DIR__ . "/../../resources/views/contact.php";
+
+?>
+
---
+++ src/Controllers/home.php
@@ -0,0 +1,8 @@
+<?php
+
+$title = "my web store";
+$heading = "Home";
+
+require __DIR__ . "/../../resources/views/index.php";
+
+?>
